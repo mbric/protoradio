e713fafbd1391174898fe9b199c8b6388403d370
Matt Brickner <mbrickner@ostusa.com>
2015-11-03 10:19:53 -0500

    mongo proof of concept. renders scrobbled tracks to landing page for now

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var mongo = require('mongoskin');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Protoradio' });
+  var db = mongo.db("mongodb://localhost:27017/protoradio", {native_parser:true});
+  db.collection('plays').find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.render('index', { title: 'Protoradio', plays: result });
+  });
 });
 
 module.exports = router;
